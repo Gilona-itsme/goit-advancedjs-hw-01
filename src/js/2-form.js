1f88b1f1bf0a3c46ef1057b3a032e8ec937e589f
Ilona <ilona.gil@outlook.com>
2026-05-25 22:21:01 +0300

    add styles

---
+++ src/js/2-form.js
@@ -0,0 +1,47 @@
+const STORAGE_KEY = 'feedback-form-state';
+
+const formData = {
+  email: '',
+  message: '',
+};
+
+const form = document.querySelector('.feedback-form');
+
+const savedData = localStorage.getItem(STORAGE_KEY);
+
+if (savedData) {
+  try {
+    const parsedData = JSON.parse(savedData);
+    formData.email = parsedData.email || '';
+    formData.message = parsedData.message || '';
+    
+    form.elements.email.value = formData.email;
+    form.elements.message.value = formData.message;
+  } catch (error) {
+    console.error("Data parsing error with localStorage", error);
+  }
+}
+
+form.addEventListener('input', event => {
+  const { name, value } = event.target;
+
+  formData[name] = value.trim();
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+});
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  if (formData.email === '' || formData.message === '') {
+    alert('Fill please all fields');
+    return;
+  }
+
+  console.log({ ...formData });
+
+
+  localStorage.removeItem(STORAGE_KEY);
+  form.reset();
+  formData.email = '';
+  formData.message = '';
+});
